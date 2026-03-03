d58b0dc8a3b6659e99c7ec5b3c4467cb1cf78cbb
Simon Matiri <symonmatyry@gmail.com>
2026-03-03 15:33:02 +0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+const page = () => {
+  return (
+    <div className="fontStyle text-5xl text-center mt-20 font-extrabold">
+      Welcome To The Future!
+    </div>
+  )
+}
+
+export default page
