cf16d14418a944522dc89708bb0d9d941c6ba74c
MatiriSimon <symonmatyry@gmail.com>
2026-05-17 10:32:58 +0000

    Changed next config.ts to allow d github codespaces communicatte with server

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "refactored-zebra-5g54xg5rjrxqf77xp-3000.app.github.dev", // The domain from your terminal
+        "localhost:3000"
+      ],
+    },
+  },
 };
 
 export default nextConfig;
