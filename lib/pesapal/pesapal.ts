d098c1cde2931854772daa60792a074696eabb3b
Matiri The Man <matirisimon3@gmail.com>
2026-09-19 01:31:22 +0300

    Wrote the first few messy lines of lib/pesapal.ts

---
+++ lib/pesapal/pesapal.ts
@@ -0,0 +1,4 @@
+export function pesapalcredentials(consumerKey: string, consumerSecret: string){
+    consumerKey = process.env.PESAPAL_CONSUMER_KEY,
+    consumerSecret = process.env.PESAPAL_CONSUMER_SECRET
+} 
