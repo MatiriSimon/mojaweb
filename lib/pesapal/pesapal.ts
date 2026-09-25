56b4fd97438a8d7e5ffe33590c91f910da8b78b7
MatiriSimon <symonmatyry@gmail.com>
2026-09-25 10:12:36 +0000

    No changes

--- lib/pesapal/pesapal.ts
+++
@@ -1,4 +0,0 @@
-export function pesapalcredentials(consumerKey: string, consumerSecret: string){
-    consumerKey = process.env.PESAPAL_CONSUMER_KEY,
-    consumerSecret = process.env.PESAPAL_CONSUMER_SECRET
-} 
